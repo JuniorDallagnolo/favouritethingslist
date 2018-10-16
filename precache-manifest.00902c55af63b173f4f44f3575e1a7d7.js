79eeaa964b810d6adddb761da113491a28c3b448
Junior Dallagnolo <ernilodjr@gmail.com>
2018-10-16 14:54:58 -0400

    Updates

---
+++ precache-manifest.00902c55af63b173f4f44f3575e1a7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60bcdd228d66c3755045",
+    "url": "/favouritethingslist/static/js/runtime~main.60bcdd22.js"
+  },
+  {
+    "revision": "78f61cf6f2ccbbec6d62",
+    "url": "/favouritethingslist/static/js/main.78f61cf6.chunk.js"
+  },
+  {
+    "revision": "7a1bf5e2c526362cf674",
+    "url": "/favouritethingslist/static/js/1.7a1bf5e2.chunk.js"
+  },
+  {
+    "revision": "78f61cf6f2ccbbec6d62",
+    "url": "/favouritethingslist/static/css/main.5b6d0a75.chunk.css"
+  },
+  {
+    "revision": "7a1bf5e2c526362cf674",
+    "url": "/favouritethingslist/static/css/1.1d699220.chunk.css"
+  },
+  {
+    "revision": "6e06722bd54747b7b46802f6157fcd1e",
+    "url": "/favouritethingslist/index.html"
+  }
+];
